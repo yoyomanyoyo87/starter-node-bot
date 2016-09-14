9d14a20ce792db14bfd4d6abe84739205b19f34a
Ian Perez <Ian Perez>
2016-09-13 23:13:27 -0400

    Uncomented message buttons

--- index.js
+++ index.js
@@ -86,31 +86,31 @@ controller.hears(['attachment'], ['direct_message', 'direct_mention'], function 
 
 controller.hears('interactive', 'direct_message', function(bot, message) {
 
-    // bot.reply(message, {
-    //     attachments:[
-    //         {
-    //             title: 'Do you want to interact with my buttons?',
-    //             callback_id: '123',
-    //             attachment_type: 'buttons',
-    //             actions: [
-    //                 {
-    //                     "name":"yes",
-    //                     "text": "Yes",
-    //                     "value": "yes",
-    //                     "type": "button",
-    //                 },
-    //                 {
-    //                     "name":"no",
-    //                     "text": "No",
-    //                     "value": "no",
-    //                     "type": "button",
-    //                 }
-    //             ]
-    //         }
-    //     ]
-    // });
+    bot.reply(message, {
+        attachments:[
+            {
+                title: 'Do you want to interact with my buttons?',
+                callback_id: '123',
+                attachment_type: 'buttons',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                        "name":"no",
+                        "text": "No",
+                        "value": "no",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
+    });
 
-    bot.reply(message, "This is a test");
+
 })
 
 
