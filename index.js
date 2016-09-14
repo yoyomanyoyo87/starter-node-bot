667ce9b38882e7cf7b60ef65aecd8fab9e4986ec
Ian Perez <Ian Perez>
2016-09-13 23:14:55 -0400

    Uncomented message button event

--- index.js
+++ index.js
@@ -119,40 +119,40 @@ controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, mess
 })
 
 
-// controller.on('interactive_message_callback', function(bot, message) {
-//
-//     // check message.actions and message.callback_id to see what action to take...
-//
-//     bot.replyInteractive(message, {
-//         text: '...',
-//         attachments: [
-//             {
-//                 title: 'My buttons',
-//                 callback_id: '123',
-//                 attachment_type: 'default',
-//                 actions: [
-//                     {
-//                         "name":"yes",
-//                         "text": "Yes!",
-//                         "value": "yes",
-//                         "type": "button",
-//                     },
-//                     {
-//                        "text": "No!",
-//                         "name": "no",
-//                         "value": "delete",
-//                         "style": "danger",
-//                         "type": "button",
-//                         "confirm": {
-//                           "title": "Are you sure?",
-//                           "text": "This will do something!",
-//                           "ok_text": "Yes",
-//                           "dismiss_text": "No"
-//                         }
-//                     }
-//                 ]
-//             }
-//         ]
-//     });
-//
-// });
+controller.on('interactive_message_callback', function(bot, message) {
+
+    // check message.actions and message.callback_id to see what action to take...
+
+    bot.replyInteractive(message, {
+        text: '...',
+        attachments: [
+            {
+                title: 'My buttons',
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes!",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                       "text": "No!",
+                        "name": "no",
+                        "value": "delete",
+                        "style": "danger",
+                        "type": "button",
+                        "confirm": {
+                          "title": "Are you sure?",
+                          "text": "This will do something!",
+                          "ok_text": "Yes",
+                          "dismiss_text": "No"
+                        }
+                    }
+                ]
+            }
+        ]
+    });
+
+});
