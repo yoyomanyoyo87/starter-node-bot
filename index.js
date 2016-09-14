c48f8d4b0767f207a9d1dc71f686cebebc6771ca
Ian Perez <Ian Perez>
2016-09-13 23:16:29 -0400

    Added confirmation text

--- index.js
+++ index.js
@@ -124,35 +124,8 @@ controller.on('interactive_message_callback', function(bot, message) {
     // check message.actions and message.callback_id to see what action to take...
 
     bot.replyInteractive(message, {
-        text: '...',
-        attachments: [
-            {
-                title: 'My buttons',
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"yes",
-                        "text": "Yes!",
-                        "value": "yes",
-                        "type": "button",
-                    },
-                    {
-                       "text": "No!",
-                        "name": "no",
-                        "value": "delete",
-                        "style": "danger",
-                        "type": "button",
-                        "confirm": {
-                          "title": "Are you sure?",
-                          "text": "This will do something!",
-                          "ok_text": "Yes",
-                          "dismiss_text": "No"
-                        }
-                    }
-                ]
-            }
-        ]
+        text: 'Gracias por confirmar'
+
     });
 
 });
